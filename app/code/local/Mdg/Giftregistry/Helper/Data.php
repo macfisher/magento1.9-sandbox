6d0147984ec3ce0367a831f1493f2debaa5e5070
macfisher <macfisher01@gmail.com>
2017-04-27 15:21:29 -0600

    Added getEventTypes Helper function

--- app/code/local/Mdg/Giftregistry/Helper/Data.php
+++ app/code/local/Mdg/Giftregistry/Helper/Data.php
@@ -2,5 +2,8 @@
 class   Mdg_Giftregistry_Helper_Data
 extends Mage_Core_Helper_Abstract
 {
-	// Default helper class
+	public function getEventTypes() {
+		$collection = Mage::getModel('mdg_giftregistry/type')->getCollection();
+		return $collection;
+	}
 }
